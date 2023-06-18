0b2de42156969ce2659d625aaa22c3ff16c925b2
Andrey <doctor_linchenko@mail.ru>
2023-06-18 13:29:05 +0300

    change str_func

--- str_func.py
+++ str_func.py
@@ -1,2 +1,20 @@
 def foo(word):
+    """
+
+    :param word:
+    :return:
+    """
     return word.upper()
+
+
+def foo2(word):
+    """
+
+    :param word:
+    :return:
+    """
+    word_list = word.split()
+    result = []
+    for word in word_list:
+        result.append(word.capitalize())
+    return ' '.join(result)
